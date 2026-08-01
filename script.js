7e04a1e7e7f49541a3195e31cd55e9a529385c8f
DaviRLuna <dluninha11@gmail.com>
2026-08-01 18:12:25 -0300

    alterações profundas no JS, em breve unificarei tudo

--- script.js
+++ script.js
@@ -20,19 +20,72 @@ const dica =
         }
     });
 
-    let estado = 1;
+    const titulo = document.getElementById("titulo");
+    const imagem = document.getElementById("Imagem");
+    const opção1 = document.getElementById("opcao1");
+    const opção2 = document.getElementById("opcao2");
 
-    const TituloElement = document.getElementById('Titulo');
-    const ImagemElement = document.getElementById('Imagem');
-    const RespostaCertaElement = document.getElementById('Resposta.C');
-    const RespostaErradaElement = document.getElementById('Resposta.E');
-    const TituloElement = document.getElementById('Titulo');
-    const botao = document.getElementById('Botaodica');
+    let CurrentQuestionIndex = 0;
+    function ShowQuestion() {
+        let perguntaAtual = quizFacilData[CurrentQuestionIndex];
 
-    function verificarResposta(RespostaSelecionada){
-        if(etapa === 1){
-            if(RespostaSelecionada === 'Errada'){
-                cons
-            }
+        document.getElementById("titulo").innerText = perguntaAtual.pergunta;
+        document.getElementById("Imagem").src = perguntaAtual.imagem;
+        document.getElementById("opcao1").innerText = perguntaAtual.opcoes[0];
+        document.getElementById("opcao2").innerText = perguntaAtual.opcoes[1];
+    }
+    const quizFacilData = [
+        {
+            pergunta: "Quem venceria em uma luta?",
+            opcoes: ["Buraco negro", "Bebe dormindo"],
+            correta: 0,
+            imagem: "src/assets/aaa.png"
+        },
+        {
+            pergunta: "Qual a diferença entre as duas imagens?",
+            opcoes: ["Nenhuma", "O cabelo"],
+            correta: 1,
+            imagem: ""
+        }
+    ];
+
+    function derrota() {
+
+        document.getElementById("Pergunta").classList.add("tela-escondida");
+        document.getElementById("respostas").classList.add("tela-escondida");
+        document.getElementById("dica").classList.add("tela-escondida");
+        document.getElementById("tela-de-derrota").classList.remove("tela-escondida");
+    }
+    function recomeço() {
+
+
+        document.getElementById("Pergunta").classList.remove("tela-escondida");
+        document.getElementById("respostas").classList.remove("tela-escondida");
+        document.getElementById("dica").classList.remove("tela-escondida");
+        document.getElementById("tela-de-derrota").classList.add("tela-escondida");
+    }
+    function ShowQuestion() {
+        let perguntaAtual = quizFacilData[CurrentQuestionIndex];
+
+        document.getElementById("titulo").innerText = perguntaAtual.pergunta;
+        document.getElementById("Imagem").src = perguntaAtual.imagem;
+        document.getElementById("opcao1").innerText = perguntaAtual.opcoes[0];
+        document.getElementById("opcao2").innerText = perguntaAtual.opcoes[1];
+    }
+    function selectOption(option) {
+
+        let perguntaAtual = quizFacilData[CurrentQuestionIndex];
+
+        if(option === perguntaAtual.correta) {
+            alert("Parabéns, você acertou!");
+            
+            CurrentQuestionIndex++;
+
+            ShowQuestion();
+        }
+        else {
+            alert("Você errou");
+            derrota();
         }
     }
+    
