f986a4dba5fc36816a39524f53a18de4e211591a
DaviRLuna <dluninha11@gmail.com>
2026-08-05 20:59:24 -0300

    alterações leves no INDEX , SEM TEMPO

--- src/Js/script-IndexHTML.js
+++ src/Js/script-IndexHTML.js
@@ -3,3 +3,15 @@ const caixaNavegação = document.getElementById("QuizPage");
 caixaNavegação.addEventListener('click', function() {
     window.location.href = 'src/pages/CentralQuiz.html'
 })
+
+const agradecimentos = document.getElementById("agradecimentos");
+const jogos = document.getElementById("Jogos")
+
+function avancar(){
+    agradecimentos.classList.remove("tela-escondida");
+    jogos.classList.add("tela-escondida");
+}
+function regredir(){
+    agradecimentos.classList.add("tela-escondida");
+    jogos.classList.remove("tela-escondida");
+}
